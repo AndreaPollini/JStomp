5c962b71dec660366a7e217f1266358cf02f8f94
15367519 <15367519@Info05pc31.CASTELLI.LOCAL>
2015-11-19 09:46:13 +0100

    eliminata riga inutile

--- JStomp/src/jstomp/JStomp.java
+++ JStomp/src/jstomp/JStomp.java
@@ -16,7 +16,7 @@ public class JStomp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("CIAO");
+        
     }
     
 }
